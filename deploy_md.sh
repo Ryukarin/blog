d0503b5628ef7c3b862ef6bc7e28cbf996201a0f
Ryuukarin <ryuukaringo@gmail.com>
2021-08-12 08:33:43 +0800

    blog_md

--- deploy_md.sh
+++ deploy_md.sh
@@ -4,22 +4,22 @@
 set -e
 
 # 生成静态文件
-yarn run docs:build
+#yarn run docs:build
 
 # 进入生成的文件夹
-cd ./dist
+#cd ./dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m 'blog'
+git commit -m 'blog_md'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:Ryukarin/blog.git master:gh-pages
+git push -f git@github.com:Ryukarin/blog.git master:main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+#cd -
